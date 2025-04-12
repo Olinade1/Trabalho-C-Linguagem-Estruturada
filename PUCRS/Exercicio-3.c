578c6f90bd1bda4472a525b947abf1144528912e
Olinade1 <danilo.ms196@hotmail.com>
2025-04-12 15:58:48 -0300

    Completo Exercicio-3.c

--- PUCRS/Exercicio-3.c
+++ PUCRS/Exercicio-3.c
@@ -11,22 +11,52 @@ O final da leitura de dados se dará com a entrada de um salário negativo. (Use
 
 int main() {
     
-    int i = 1; // contador
-    float sal = 0, salAdicao = 0, salMaior;// salario, somatorio, valor maior
+    int i = 1, salAteCem = 0, son = 0; 
+    // contador, salario até cem, nº de filhos
+    float sal = 0, salAdicao = 0, salMaior = 0, sonAdicao; 
+    // salario, somatorio, salario maior, somatorio de filhos
     
    while (sal>=0){
        
     printf("Informe salário: ");
     scanf("%f",&sal);
-        
-    salAdicao = salAdicao + sal; 
     
-    i++;
+    if(sal<0)
+        break;
+        
+    salAdicao = salAdicao + sal;
+    
+    if(sal>salMaior)
+        salMaior = sal;
+    
+    if (sal<=100)
+        salAteCem += 1;
+        
+    printf("Informe nº de filhos: ");
+    scanf("%d",&son);
+    
+    sonAdicao += son; // sonAdicao = sonAdicao + son;
+    
+    printf("\n");
+    
+    i++; // i = i + 1
     
    }
    
-   printf("\nSoma dos salários: %f", salAdicao);
-   printf("\nMédia dos salários: %f", salAdicao/(i-1));
-    
-    return 0;
+   printf("\n=============================");
+   
+   printf("\n\nQnt de pessoas pesquisadas: %d", i-1);
+   
+   printf("\n\nSoma dos salários: %.2f", salAdicao);
+   printf("\nMédia dos salários: %.2f", salAdicao/(i-1));
+   printf("\nMaior salário: %.2f", salMaior);
+   
+   printf("\n\nQnt de pessoas com salário até 100 reais: %d", salAteCem);
+   printf("\nPorcentagem daqueles que recebem menos de 100 reais: %.2f por cento", 100.0 * salAteCem/(i-1));
+   
+   printf("\n\nNº de filhos total: %.0f",sonAdicao);
+   printf("\nMédia do Nº de filhos: %.2f",sonAdicao/(i-1));
+   
+   return 0;
+   
 }
